edd6728495b9ae0481462fce8f26bc0c0b0eb8b3
Anthonyny019 <anthony.genesino@etec.sp.gov.br>
2021-09-16 02:45:06 +0000

    Aula 05 - Hooks

--- hooks/src/views/contents/NaoEncontrado.jsx
+++ hooks/src/views/contents/NaoEncontrado.jsx
@@ -1,9 +1,11 @@
+import './NaoEncontrado.css'
 import React from "react"
 
 const NaoEncontrado = props => (
-    <div>
-        <h1>Página não encontrada.</h1>
+    <div className="erro">
+        <h1><u>Página não encontrada.</u>😞 </h1>
     </div>
+
 )
 
 export default NaoEncontrado
